3ad7a1874f223617b8cc0ff4e1681d757d3c9cf6
hongyue-qiu <hongyue.qiu@thoughtworks.com>
2022-08-02 18:15:35 +0800

    todo item checkbox select handle

--- src/pages/todoPanel/todoItem/todoItem.tsx
+++ src/pages/todoPanel/todoItem/todoItem.tsx
@@ -2,16 +2,22 @@ import React from "react";
 import "./todoItem.css"
 
 interface TodoItemProps {
+    index?: number,
     isChecked: boolean,
     description: string,
-    priority: 'normal' | 'emergency' | 'important'
+    priority: 'normal' | 'emergency' | 'important',
+    onCheckboxClicked: () => void,
 }
 
-const TodoItem = ({isChecked, description, priority}:TodoItemProps) => {
+const TodoItem = ({isChecked, description, priority, onCheckboxClicked}:TodoItemProps) => {
+
+    const handleClicked = () => {
+        onCheckboxClicked()
+    }
 
     return <div className="todo-item">
         <div className="todo-item-content">
-            <input type="checkbox" id="index" className="todo-item-content-checkbox" checked={isChecked} />
+            <input type="checkbox" id="index" className="todo-item-content-checkbox" checked={isChecked} onClick={handleClicked} />
             <label className="todo-item-content-description">{description}</label>
         </div>
         <select className="todo-item-priority" value={priority}>
--- src/pages/todoPanel/todoPanel.tsx
+++ src/pages/todoPanel/todoPanel.tsx
@@ -1,18 +1,29 @@
 import React from "react";
 import "./todoPanel.css"
-import {useAppSelector} from "../../app/hooks";
-import {selectTodos} from "../../reducer/todoSlice";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { selectTodos, markEvent } from "../../reducer/todoSlice";
 import TodoItem from "./todoItem/todoItem";
 
 const TodoPanel = () => {
 
+    const dispatch = useAppDispatch()
+
     const todos = useAppSelector(selectTodos)
+    const handleTodoItemCheckboxClicked = (index: number) => {
+        dispatch(markEvent(index))
+    }
 
     return <div className="todo-panel">
         <section className="todo-list">
             <h3>todo list</h3>
-            {todos.todos.map(item =>
-                <TodoItem isChecked={item.completed} description={item.text} priority={item.priority} />
+            {todos.todos
+                .map(item =>
+                    <TodoItem
+                        isChecked={item.completed}
+                        description={item.text}
+                        onCheckboxClicked={() => handleTodoItemCheckboxClicked(item.index ?? 0)}
+                        priority={item.priority}
+                    />
                 )
             }
         </section>
