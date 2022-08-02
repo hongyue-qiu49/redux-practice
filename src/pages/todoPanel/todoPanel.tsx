cfeba06a23dcc7f0a9043970807eb999466e2ebe
hongyue-qiu <hongyue.qiu@thoughtworks.com>
2022-08-02 11:18:54 +0800

    add todo list with one item

--- src/pages/todoPanel/todoPanel.tsx
+++ src/pages/todoPanel/todoPanel.tsx
@@ -7,8 +7,15 @@ const TodoPanel = () => {
         <section className="todo-list">
             <h3>todo list</h3>
             <div className="todo-item">
-                <input type="checkbox" id="index"/>
-                <label>learn redux</label>
+                <div className="todo-item-content">
+                    <input type="checkbox" id="index"  className="todo-item-content-checkbox" />
+                    <label className="todo-item-content-description">learn redux</label>
+                </div>
+                <select className="todo-item-priority">
+                    <option>normal</option>
+                    <option>emergency</option>
+                    <option>important</option>
+                </select>
             </div>
         </section>
     </div>
