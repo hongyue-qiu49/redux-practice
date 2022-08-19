08b08effaee909836a8a67913f3ee7f796e969fa
hongyue-qiu <hongyue.qiu@thoughtworks.com>
2022-08-19 11:26:55 +0800

    eslint change and fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
         '@typescript-eslint/explicit-function-return-type': 0,
         '@typescript-eslint/no-non-null-assertion': 0,
         '@typescript-eslint/no-var-requires': 0,
+        '@typescript-eslint/no-floating-promises': 0,
         'no-debugger': 0
       }
     }
