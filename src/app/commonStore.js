6ff0c8868bbad3e04af8cb30b3d0076f14773643
hongyue-qiu <hongyue.qiu@thoughtworks.com>
2022-09-09 12:52:02 +0800

    [feature]: add redux logger

--- src/app/commonStore.js
+++ src/app/commonStore.js
@@ -4,6 +4,7 @@ import { todoReducer } from '../reducer/todoReducer/todoCommonReducer'
 import { counterReducer } from '../reducer/counterReducer/counterCommonReducer'
 import { initialCounterState } from '../type/counter'
 import { initialTodoState } from '../type/todo'
+import { logger } from 'redux-logger'
 
 const rootReducer = combineReducers({ todo: todoReducer, counter: counterReducer })
 
@@ -13,5 +14,5 @@ export const store = createStore(
     counter: initialCounterState,
     todo: initialTodoState
   },
-  applyMiddleware(thunkMiddleware)
+  applyMiddleware(thunkMiddleware, logger)
 )
